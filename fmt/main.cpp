4e64c66ae79d26da55316d001a56b63d2965f1b7
Daniel Shevchenko <dshev@dshev-macbookpro.roam.corp.google.com>
2014-09-05 09:09:14 -0700

    Initial Commit

---
+++ fmt/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fmt
+//
+//  Created by Daniel Shevchenko on 9/5/14.
+//  Copyright (c) 2014 Daniel Shevchenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
